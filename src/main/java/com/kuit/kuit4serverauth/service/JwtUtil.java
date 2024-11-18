d676a751fe8d557a2872359000cf2e24f7273a53
윤정 <70215620+yunjeongiya@users.noreply.github.com>
2024-11-18 21:12:27 +0900

    fix: update secret key length

--- src/main/java/com/kuit/kuit4serverauth/service/JwtUtil.java
+++ src/main/java/com/kuit/kuit4serverauth/service/JwtUtil.java
@@ -11,7 +11,7 @@ import java.util.Date;
 
 @Component
 public class JwtUtil {
-    private final String secret = "mysecretkey";
+    private final String secret = "ZGVtbzEyMzQ1Njc4OWFiY2RlZmdoaWprbG1ub3BxcnN0dXZ3eHl6YWJjZGVmZw==";
     private final long expirationMs = 3600000; // 1 hour
 
     public String generateToken(String username, String role) {
