d5a244cc30a9e0d5f82b34b0ed14a897c2289e30
Yun531 <jaeyoongh315@gmail.com>
2024-11-22 17:39:22 +0900

    dto

---
+++ src/main/java/com/kuit/kuit4serverauth/dto/LoginResponse.java
@@ -0,0 +1,14 @@
+package com.kuit.kuit4serverauth.dto;
+
+import lombok.Getter;
+
+@Getter
+public class LoginResponse {
+    private final String accessToken;
+    private final String refreshToken;
+
+    public LoginResponse(String accessToken, String refreshToken) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+    }
+}
