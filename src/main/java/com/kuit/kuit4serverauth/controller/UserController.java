cabd1d96cab0dc4762edd39a0d46f27fe6cbf24a
Yun531 <jaeyoongh315@gmail.com>
2024-12-02 21:34:02 +0900

    .

--- src/main/java/com/kuit/kuit4serverauth/controller/UserController.java
+++ src/main/java/com/kuit/kuit4serverauth/controller/UserController.java
@@ -2,9 +2,7 @@ package com.kuit.kuit4serverauth.controller;
 
 import com.kuit.kuit4serverauth.dto.UserDetail;
 import com.kuit.kuit4serverauth.service.UserService;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
